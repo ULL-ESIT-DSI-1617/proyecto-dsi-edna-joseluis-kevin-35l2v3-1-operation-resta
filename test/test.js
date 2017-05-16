3353b276d36b36ee80ae9f60a1a0b8f915e624a5
alu0100821390 <alu0100821390@ull.edu.es>
2017-05-16 21:15:25 +0100

    Añadir pruebas mocha chai closes #3

---
+++ test/test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var chai = require("chai");
+var assert = chai.assert;
+var Operation = require("@ull-edna-joseluis-kevin-35l2/ull-operation");
+var Resta = require("../index.js");
+
+describe('resta', function() {
+	it('2-3', function() {
+		var result = Operation.operate("2-3");
+		assert.equal(result, -1.00);
+	});
+	it('2e2-3', function() {
+		var result = Operation.operate("2e2-3");
+		assert.equal(result, 197);
+	});
+});
